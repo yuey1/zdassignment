0386476bdc2b1501b8292f08ca346276f016601e
yuey1 <yuey1@andrew.cmu.edu>
2013-05-26 19:26:23 -0400

    Added a function to select top n entries according to some keys

--- src/zdassignment.py
+++ src/zdassignment.py
@@ -61,5 +61,61 @@ def read_files(path):
                 else:
                     word_count[w] = 1
     return word_count
+def rank_selection(input_list,sort_key,lo,hi,n):
+    """
+    partially sort the list according to some keys using pivoting
+    technique, the running time is O(log n)
+    @param input_list, a list of tuples, in our case it would be
+    (word, count)
+    @param sort_key, the index of keys in the tuple we want to sort
+    @param lo, define the left bound of the list we want to sort
+    @param hi, define the right bount of the list we want to sort
+    @param n, the number of entries we want, in our case is 10
+    @return none, it sorts the list in place
+    """
+    # input integrity checking
+    if len(input_list) == 0:
+        return
+    if sort_key < 0 or sort_key >= len(input_list[0]):
+        print "invalid sort_key"
+        return 
+    if hi - lo + 1 < n:
+        return 
+    if lo < 0 or hi >= len(input_list):
+        return 
+    if lo >= hi:
+        return
+    #base condition
+    if n == 0:
+        return 
+    pivot = input_list[hi][sort_key]
+    lo_runner = lo
+    hi_runner = hi - 1
+    while lo_runner < hi_runner:
+        if input_list[lo_runner][sort_key] < pivot:
+            lo_runner += 1
+        else:
+            #swap the lo and hi
+            tmp_tuple = input_list[lo_runner]
+            input_list[lo_runner] = input_list[hi_runner]
+            input_list[hi_runner] = tmp_tuple
+            hi_runner -= 1
+    # at this point, all tuples with keys which are greater than
+    # or equal to pivot will be at the right hand side, then swap 
+    # the pivot and go to the recursive step
+    pivot_idx = lo_runner # lo_runner = hi_runner
+    if input_list[lo_runner][sort_key] < pivot:
+        pivot_idx = lo_runner + 1
+    tmp_tuple = input_list[hi]
+    input_list[hi] = input_list[pivot_idx]
+    input_list[pivot_idx] = tmp_tuple
+
+    target_n = hi - pivot_idx + 1
+    if target_n == n:
+        return 
+    elif target_n < n:
+        rank_selection(input_list,sort_key,lo,pivot_idx-1,n-target_n)
+    else:
+        rank_selection(input_list,sort_key,pivot_idx,hi,n)
 if __name__ == "__main__":
     print(count_doc_number(sys.argv[1]))
